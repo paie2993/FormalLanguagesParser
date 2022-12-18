b110cddf96e2d52ecfcf0d369e9fedac48637acb
AndreiSaramet <andreisaramet21@gmail.com>
2022-12-18 15:33:08 +0200

    model production

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/production/Production.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/production/Production.java
@@ -1,50 +1,50 @@
 package org.compilers.grammar.model.production;
 
-import org.compilers.grammar.model.vocab.Vocab;
+import org.compilers.grammar.model.vocabulary.NonTerminal;
+import org.compilers.grammar.model.vocabulary.Symbol;
 
 import java.util.List;
 import java.util.Objects;
 
 public final class Production {
+    public static final String SIDES_SEPARATOR = " -> ";
 
+    private final List<Symbol> leftSide;
 
-    private static final String sidesSeparator = " -> ";
+    private final List<Symbol> rightSide;
 
-    private final List<Vocab> left;
+    public Production(final List<Symbol> leftSide, final List<Symbol> rightSide) {
+        Production.validateLeftSide(leftSide);
+        Production.validateSide(rightSide);
 
-    private final List<Vocab> right;
-
-    public Production(final List<Vocab> left, final List<Vocab> right) {
-        validateSide(left);
-        validateSide(right);
-
-        this.left = left;
-        this.right = right;
+        this.leftSide = leftSide;
+        this.rightSide = rightSide;
     }
 
-    public List<Vocab> left() {
-        return left;
+    public List<Symbol> leftSide() {
+        return leftSide;
     }
 
-    public List<Vocab> right() {
-        return right;
+    public List<Symbol> rightSide() {
+        return rightSide;
     }
 
-    @Override
-    public String toString() {
-        return prepareListForPrinting(left) + sidesSeparator + prepareListForPrinting(right);
-    }
-
-    private static String prepareListForPrinting(final List<Vocab> list) {
-        final var builder = new StringBuilder();
-        list.forEach(builder::append);
-        return builder.toString();
-    }
-
-    private static void validateSide(final List<Vocab> side) {
+    private static void validateSide(final List<Symbol> side) {
         Objects.requireNonNull(side);
         if (side.isEmpty()) {
             throw new IllegalArgumentException("Side of production can't be empty");
+        }
+    }
+
+    private static void validateLeftSide(final List<Symbol> leftSide) {
+        Production.validateSide(leftSide);
+        Production.containsNonTerminal(leftSide);
+    }
+
+    private static void containsNonTerminal(final List<Symbol> side) {
+        Objects.requireNonNull(side);
+        if (side.stream().noneMatch(symbol -> NonTerminal.class.equals(symbol.getClass()))) {
+            throw new IllegalArgumentException("Left side of production must contain at least one non-terminal symbol");
         }
     }
 }
--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/production/ProductionBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/production/ProductionBuilder.java
@@ -1,10 +1,9 @@
 package org.compilers.grammar.model.production;
 
-import org.compilers.grammar.model.Grammar;
-import org.compilers.grammar.model.GrammarReader;
-import org.compilers.grammar.model.vocab.non_terminal.NonTerminal;
-import org.compilers.grammar.model.vocab.terminal.Terminal;
-import org.compilers.grammar.model.vocab.Vocab;
+import org.compilers.grammar.model.grammar.Grammar;
+import org.compilers.grammar.model.vocabulary.NonTerminal;
+import org.compilers.grammar.model.vocabulary.Symbol;
+import org.compilers.grammar.model.vocabulary.Terminal;
 
 import java.util.Arrays;
 import java.util.List;
@@ -13,9 +12,7 @@ import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-public final class ProductionBuilder {
-
-    private static final String SIDES_SEPARATOR = " -> ";
+public class ProductionBuilder {
     private static final String SYMBOLS_SEPARATOR = "`";
 
     private final Convention convention;
@@ -24,11 +21,15 @@ public final class ProductionBuilder {
         this.convention = new Convention(nonTerminals, terminals);
     }
 
-    public Production buildProduction(final String productionString) {
-        final String[] sides = productionString.split(SIDES_SEPARATOR);
+    public ProductionBuilder(final Grammar grammar) {
+        this.convention = new Convention(grammar.nonTerminals(), grammar.terminals());
+    }
 
-        final List<Vocab> left = buildLeftSide(sides[0]);
-        final List<Vocab> right = buildRightSide(sides[1]);
+    public Production buildProduction(final String productionString) {
+        final String[] sides = productionString.split(Production.SIDES_SEPARATOR);
+
+        final List<Symbol> left = buildLeftSide(sides[0]);
+        final List<Symbol> right = buildRightSide(sides[1]);
 
         return new Production(left, right);
     }
@@ -36,19 +37,16 @@ public final class ProductionBuilder {
     /**
      * Builds the left side of the production
      */
-    private List<Vocab> buildLeftSide(final String sideString) {
+    private List<Symbol> buildLeftSide(final String sideString) {
         return buildSide(sideString);
     }
 
     /**
      * Builds the right side of the production, with the special case of empty production
      */
-    private List<Vocab> buildRightSide(final String sideString) {
+    private List<Symbol> buildRightSide(final String sideString) {
         final var epsilonOptional = specialCase(sideString);
-        if (epsilonOptional.isPresent()) {
-            return List.of(epsilonOptional.get());
-        }
-        return buildSide(sideString);
+        return epsilonOptional.<List<Symbol>>map(List::of).orElseGet(() -> buildSide(sideString));
     }
 
     private Optional<Terminal> specialCase(final String rightSide) {
@@ -58,7 +56,7 @@ public final class ProductionBuilder {
         return Optional.empty();
     }
 
-    private List<Vocab> buildSide(final String sideString) {
+    private List<Symbol> buildSide(final String sideString) {
         final String[] symbols = processSymbols(sideString);
         return convertSymbols(symbols);
     }
@@ -73,32 +71,37 @@ public final class ProductionBuilder {
     /**
      * Converts an array of vocab representations to their corresponding terminals and non-terminals
      */
-    private List<Vocab> convertSymbols(final String[] symbols) {
-        return Arrays.stream(symbols).map(this::convertSymbol).collect(Collectors.toList());
+    private List<Symbol> convertSymbols(final String[] symbols) {
+        return Arrays.stream(symbols)
+                .map(this::convertSymbol)
+                .collect(Collectors.toList());
     }
 
-    private Vocab convertSymbol(final String symbol) {
-        final Function<String, Vocab> converter = convention.getConverter(symbol);
-        return converter.apply(symbol);
+    private Symbol convertSymbol(final String symbol) {
+        return this.convention.getConverter(symbol).apply(symbol);
     }
 
-    public static final class Convention {
 
+    private static class Convention {
         private final Set<String> nonTerminalsValues;
         private final Set<String> terminalsValues;
 
         public Convention(final Set<NonTerminal> nonTerminals, final Set<Terminal> terminals) {
-            this.terminalsValues = terminals.stream().map(Terminal::value).collect(Collectors.toUnmodifiableSet()); // unbound
-            this.nonTerminalsValues = nonTerminals.stream().map(NonTerminal::value).collect(Collectors.toUnmodifiableSet()); // unbound
+            this.nonTerminalsValues = nonTerminals.stream()
+                    .map(Symbol::value)
+                    .collect(Collectors.toUnmodifiableSet());
+            this.terminalsValues = terminals.stream()
+                    .map(Symbol::value)
+                    .collect(Collectors.toUnmodifiableSet());
         }
 
-        private Function<String, Vocab> getConverter(final String string) {
+        private Function<String, Symbol> getConverter(final String string) {
             if (terminalsValues.contains(string)) { // terminal
                 return Terminal::new;
             } else if (nonTerminalsValues.contains(string)) { // non-terminal
                 return NonTerminal::new;
             }
-            throw new IllegalArgumentException("The string " + string + " does not follow any convention");
+            throw new IllegalArgumentException(String.format("The string %s does not follow any convention", string));
         }
     }
 }
