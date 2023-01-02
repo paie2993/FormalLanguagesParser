e9121abdecdf4a3c2113341d73c78ee5552d0a29
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-02 13:16:40 +0200

    vocabulary symbol update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/vocabulary/Symbol.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/vocabulary/Symbol.java
@@ -1,39 +1,5 @@
 package org.compilers.grammar.model.vocabulary;
 
-import java.util.Objects;
-
-public abstract class Symbol {
-
-    private final String value;
-
-    public Symbol(final String value) {
-        Objects.requireNonNull(value);
-
-        this.value = value;
-    }
-
-    public String value() {
-        return this.value;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || this.getClass() != o.getClass())
-            return false;
-        final Symbol other = (Symbol) o;
-        return Objects.equals(other.value, this.value);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(value);
-    }
-
-    @Override
-    public String toString() {
-        return this.value;
-    }
+public interface Symbol {
+    String value();
 }
