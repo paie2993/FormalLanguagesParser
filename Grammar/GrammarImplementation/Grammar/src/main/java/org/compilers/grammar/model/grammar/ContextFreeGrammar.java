0c3e36d6ceb674aeedb4e046c2bd1e85532232a4
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-04 16:32:08 +0200

    grammar context free interface

---
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/ContextFreeGrammar.java
@@ -0,0 +1,18 @@
+package org.compilers.grammar.model.grammar;
+
+import org.compilers.grammar.model.grammar.context_dependent.ContextDependentGrammar;
+import org.compilers.grammar.model.grammar.production.context_free.ContextFreeProduction;
+import org.compilers.grammar.model.vocabulary.Symbol;
+
+import java.util.Set;
+
+public interface ContextFreeGrammar extends ContextDependentGrammar {
+    @Override
+    Set<? extends ContextFreeProduction> productions();
+
+    @Override
+    Set<? extends ContextFreeProduction> haveSymbolInRightSide(final Symbol symbol);
+
+    @Override
+    Set<? extends ContextFreeProduction> haveSymbolInLeftSide(final Symbol symbol);
+}
