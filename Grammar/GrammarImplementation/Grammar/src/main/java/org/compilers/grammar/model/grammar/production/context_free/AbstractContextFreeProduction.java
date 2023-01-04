f52904c65f2c98c64f6352c69139e2bd784cc160
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-04 19:57:21 +0200

    grammar production added abstract context-free production

---
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/production/context_free/AbstractContextFreeProduction.java
@@ -0,0 +1,22 @@
+package org.compilers.grammar.model.grammar.production.context_free;
+
+import org.compilers.grammar.model.grammar.production.AbstractProduction;
+import org.compilers.grammar.model.vocabulary.Symbol;
+import org.compilers.grammar.model.vocabulary.nonterminal.NonTerminal;
+
+import java.util.List;
+
+public abstract class AbstractContextFreeProduction extends AbstractProduction implements ContextFreeProduction {
+    public AbstractContextFreeProduction(List<Symbol> leftSide, List<Symbol> rightSide) {
+        super(leftSide, rightSide);
+    }
+
+    public AbstractContextFreeProduction(NonTerminal leftSide, List<Symbol> rightSide) {
+        super(List.of(leftSide), rightSide);
+    }
+
+    @Override
+    public NonTerminal leftSideNonTerminal() {
+        return (NonTerminal) this.leftSide.get(0);
+    }
+}
