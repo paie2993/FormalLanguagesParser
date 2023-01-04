11dcda9b698e378df7fecb2f6987074b1863a555
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-04 16:21:04 +0200

    grammar context dependent interface

---
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_dependent/ContextDependentGrammar.java
@@ -0,0 +1,18 @@
+package org.compilers.grammar.model.grammar.context_dependent;
+
+import org.compilers.grammar.model.grammar.production.context_dependent.ContextDependentProduction;
+import org.compilers.grammar.model.grammar.unrestricted.UnrestrictedGrammar;
+import org.compilers.grammar.model.grammar.vocabulary.Symbol;
+
+import java.util.Set;
+
+public interface ContextDependentGrammar extends UnrestrictedGrammar {
+    @Override
+    Set<? extends ContextDependentProduction> productions();
+
+    @Override
+    Set<? extends ContextDependentProduction> haveSymbolInRightSide(final Symbol symbol);
+
+    @Override
+    Set<? extends ContextDependentProduction> haveSymbolInLeftSide(final Symbol symbol);
+}
