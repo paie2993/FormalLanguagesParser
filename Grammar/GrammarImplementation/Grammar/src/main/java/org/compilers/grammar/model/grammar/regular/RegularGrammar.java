1ad8479d1611fcbdae927e6d3073eaabbc27ec52
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-04 22:22:43 +0200

    grammar regular interface update + implementation

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/regular/RegularGrammar.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/regular/RegularGrammar.java
@@ -3,6 +3,7 @@ package org.compilers.grammar.model.grammar.regular;
 import org.compilers.grammar.model.grammar.production.regular.RegularProduction;
 import org.compilers.grammar.model.grammar.right_linear.RightLinearGrammar;
 import org.compilers.grammar.model.vocabulary.Symbol;
+import org.compilers.grammar.model.vocabulary.nonterminal.NonTerminal;
 
 import java.util.Set;
 
@@ -15,4 +16,22 @@ public interface RegularGrammar extends RightLinearGrammar {
 
     @Override
     Set<? extends RegularProduction> haveSymbolInLeftSide(final Symbol symbol);
+
+    static void validateGrammar(
+            final Set<? extends RegularProduction> productions,
+            final NonTerminal startSymbol
+    ) {
+        final boolean startSymbolGoesInEpsilon = productions
+                .stream()
+                .filter(production -> startSymbol.equals(production.leftSideNonTerminal()))
+                .anyMatch(production -> production.rightSide().isEmpty());
+        if (startSymbolGoesInEpsilon) {
+            final boolean startSymbolAppearsInRightSide = productions
+                    .stream()
+                    .anyMatch(production -> production.hasSymbolInRightSide(startSymbol));
+            if (startSymbolAppearsInRightSide) {
+                throw new IllegalArgumentException("The given grammar is not regular");
+            }
+        }
+    }
 }
