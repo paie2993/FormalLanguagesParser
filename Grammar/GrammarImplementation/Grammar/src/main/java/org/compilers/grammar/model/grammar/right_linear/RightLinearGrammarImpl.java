304e25d53cb81915863175bb81be9affbcdb0260
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-04 22:15:07 +0200

    grammar right-linear implementation

---
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/right_linear/RightLinearGrammarImpl.java
@@ -0,0 +1,18 @@
+package org.compilers.grammar.model.grammar.right_linear;
+
+import org.compilers.grammar.model.grammar.context_free.AbstractContextFreeGrammar;
+import org.compilers.grammar.model.grammar.production.right_linear.RightLinearProduction;
+import org.compilers.grammar.model.vocabulary.nonterminal.NonTerminal;
+import org.compilers.grammar.model.vocabulary.terminal.Terminal;
+
+import java.util.Set;
+
+public class RightLinearGrammarImpl extends AbstractContextFreeGrammar<RightLinearProduction> implements RightLinearGrammar {
+    public RightLinearGrammarImpl(
+            final Set<? extends NonTerminal> nonTerminals,
+            final Set<? extends Terminal> terminals,
+            final Set<? extends RightLinearProduction> productions,
+            final NonTerminal startSymbol) {
+        super(nonTerminals, terminals, productions, startSymbol);
+    }
+}
