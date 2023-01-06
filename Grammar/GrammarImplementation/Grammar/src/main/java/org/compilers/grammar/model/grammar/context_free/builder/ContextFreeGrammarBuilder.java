2a6f3db3f4aa54a3da4d1b235b16d080c2f88d77
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:09:30 +0200

    grammar context-free builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_free/builder/ContextFreeGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_free/builder/ContextFreeGrammarBuilder.java
@@ -1,14 +1,13 @@
 package org.compilers.grammar.model.grammar.context_free.builder;
 
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
+import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
 import org.compilers.grammar.model.grammar.context_free.ContextFreeGrammar;
 import org.compilers.grammar.model.grammar.context_free.ContextFreeGrammarImpl;
-import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.context_free.ContextFreeProduction;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class ContextFreeGrammarBuilder extends AbstractGrammarBuilder<ContextFreeProduction, ContextFreeGrammar<? extends ContextFreeProduction>> {
     public ContextFreeGrammarBuilder() {
@@ -19,19 +18,11 @@ public class ContextFreeGrammarBuilder extends AbstractGrammarBuilder<ContextFre
         this.partialBuild();
 
         final ProductionBuilder<? extends ContextFreeProduction> productionBuilder = ContextFreeProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends ContextFreeProduction> productions = prepareProductions(this.grammar.productions(), productionBuilder);
+        final Set<? extends ContextFreeProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
         final ContextFreeGrammar<? extends ContextFreeProduction> contextFreeGrammar = new ContextFreeGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
 
         this.eraseAll();
 
         return contextFreeGrammar;
-    }
-
-    private static Set<? extends ContextFreeProduction> prepareProductions(final Set<? extends Production> productions, final ProductionBuilder<? extends ContextFreeProduction> builder) {
-        return productions
-                .stream()
-                .map(builder::production)
-                .map(ProductionBuilder::build)
-                .collect(Collectors.toUnmodifiableSet());
     }
 }
