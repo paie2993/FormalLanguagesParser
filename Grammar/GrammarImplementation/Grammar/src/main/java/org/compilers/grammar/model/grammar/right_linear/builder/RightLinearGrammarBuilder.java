e7434276c490c7b8e41bbff689a3eac1a09ad856
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:37:36 +0200

    grammar right-linear builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/right_linear/builder/RightLinearGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/right_linear/builder/RightLinearGrammarBuilder.java
@@ -1,13 +1,15 @@
 package org.compilers.grammar.model.grammar.right_linear.builder;
 
+import org.compilers.grammar.model.grammar.Grammar;
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
-import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
+import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.right_linear.RightLinearProduction;
 import org.compilers.grammar.model.grammar.right_linear.RightLinearGrammar;
 import org.compilers.grammar.model.grammar.right_linear.RightLinearGrammarImpl;
 
 import java.util.Set;
+import java.util.function.BiFunction;
 
 public class RightLinearGrammarBuilder extends AbstractGrammarBuilder<RightLinearProduction, RightLinearGrammar<? extends RightLinearProduction>> {
     public RightLinearGrammarBuilder() {
@@ -15,14 +17,8 @@ public class RightLinearGrammarBuilder extends AbstractGrammarBuilder<RightLinea
 
     @Override
     public RightLinearGrammar<? extends RightLinearProduction> build() {
-        this.partialBuild();
-
-        final ProductionBuilder<? extends RightLinearProduction> productionBuilder = RightLinearProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends RightLinearProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
-        final RightLinearGrammar<? extends RightLinearProduction> rightLinearGrammar = new RightLinearGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
-
-        this.eraseAll();
-
-        return rightLinearGrammar;
+        final BiFunction<Grammar<? extends Production>, Set<? extends RightLinearProduction>, RightLinearGrammar<? extends RightLinearProduction>> create = (grammar, productions) -> new RightLinearGrammarImpl(grammar.nonTerminals(), grammar.terminals(), productions, grammar.startSymbol());
+        final ProductionBuilder<? extends RightLinearProduction> productionBuilder = RightLinearProduction.builder();
+        return this.partialBuild(create, productionBuilder);
     }
 }
