879d2c3054e2812ae44a62fb3e5537c2f4d049b7
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:08:40 +0200

    grammar context-dependent builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_dependent/builder/ContextDependentGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_dependent/builder/ContextDependentGrammarBuilder.java
@@ -1,14 +1,13 @@
 package org.compilers.grammar.model.grammar.context_dependent.builder;
 
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
+import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
 import org.compilers.grammar.model.grammar.context_dependent.ContextDependentGrammar;
 import org.compilers.grammar.model.grammar.context_dependent.ContextDependentGrammarImpl;
-import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.context_dependent.ContextDependentProduction;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class ContextDependentGrammarBuilder extends AbstractGrammarBuilder<ContextDependentProduction, ContextDependentGrammar<? extends ContextDependentProduction>> {
     public ContextDependentGrammarBuilder() {
@@ -19,19 +18,11 @@ public class ContextDependentGrammarBuilder extends AbstractGrammarBuilder<Conte
         this.partialBuild();
 
         final ProductionBuilder<? extends ContextDependentProduction> productionBuilder = ContextDependentProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends ContextDependentProduction> productions = prepareProductions(this.grammar.productions(), productionBuilder);
+        final Set<? extends ContextDependentProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
         final ContextDependentGrammar<? extends ContextDependentProduction> contextDependentGrammar = new ContextDependentGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
 
         this.eraseAll();
 
         return contextDependentGrammar;
-    }
-
-    private static Set<? extends ContextDependentProduction> prepareProductions(final Set<? extends Production> productions, final ProductionBuilder<? extends ContextDependentProduction> builder) {
-        return productions
-                .stream()
-                .map(builder::production)
-                .map(ProductionBuilder::build)
-                .collect(Collectors.toUnmodifiableSet());
     }
 }
