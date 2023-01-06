7fc4e98da3694b33f674b97e16fd6c7d51f87801
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:32:48 +0200

    grammar context-dependent builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_dependent/builder/ContextDependentGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/context_dependent/builder/ContextDependentGrammarBuilder.java
@@ -1,13 +1,15 @@
 package org.compilers.grammar.model.grammar.context_dependent.builder;
 
+import org.compilers.grammar.model.grammar.Grammar;
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
-import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
 import org.compilers.grammar.model.grammar.context_dependent.ContextDependentGrammar;
 import org.compilers.grammar.model.grammar.context_dependent.ContextDependentGrammarImpl;
+import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.context_dependent.ContextDependentProduction;
 
 import java.util.Set;
+import java.util.function.BiFunction;
 
 public class ContextDependentGrammarBuilder extends AbstractGrammarBuilder<ContextDependentProduction, ContextDependentGrammar<? extends ContextDependentProduction>> {
     public ContextDependentGrammarBuilder() {
@@ -15,14 +17,8 @@ public class ContextDependentGrammarBuilder extends AbstractGrammarBuilder<Conte
 
     @Override
     public ContextDependentGrammar<? extends ContextDependentProduction> build() {
-        this.partialBuild();
-
-        final ProductionBuilder<? extends ContextDependentProduction> productionBuilder = ContextDependentProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends ContextDependentProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
-        final ContextDependentGrammar<? extends ContextDependentProduction> contextDependentGrammar = new ContextDependentGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
-
-        this.eraseAll();
-
-        return contextDependentGrammar;
+        final BiFunction<Grammar<? extends Production>, Set<? extends ContextDependentProduction>, ContextDependentGrammar<? extends ContextDependentProduction>> create = (grammar, productions) -> new ContextDependentGrammarImpl(grammar.nonTerminals(), grammar.terminals(), productions, grammar.startSymbol());
+        final ProductionBuilder<? extends ContextDependentProduction> productionBuilder = ContextDependentProduction.builder();
+        return this.partialBuild(create, productionBuilder);
     }
 }
