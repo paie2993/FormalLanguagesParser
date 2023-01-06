4d765c000e6a85d5998fbf7c6b982a7c4dea2bfd
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:30:13 +0200

    grammar unrestricted builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/unrestricted/builder/UnrestrictedGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/unrestricted/builder/UnrestrictedGrammarBuilder.java
@@ -1,10 +1,15 @@
 package org.compilers.grammar.model.grammar.unrestricted.builder;
 
+import org.compilers.grammar.model.grammar.Grammar;
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
+import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.unrestricted.UnrestrictedProduction;
 import org.compilers.grammar.model.grammar.unrestricted.UnrestrictedGrammar;
 import org.compilers.grammar.model.grammar.unrestricted.UnrestrictedGrammarImpl;
+
+import java.util.Set;
+import java.util.function.BiFunction;
 
 public class UnrestrictedGrammarBuilder extends AbstractGrammarBuilder<UnrestrictedProduction, UnrestrictedGrammar<? extends UnrestrictedProduction>> {
     public UnrestrictedGrammarBuilder() {
@@ -12,7 +17,8 @@ public class UnrestrictedGrammarBuilder extends AbstractGrammarBuilder<Unrestric
 
     @Override
     public UnrestrictedGrammar<? extends UnrestrictedProduction> build() {
+        final BiFunction<Grammar<? extends Production>, Set<? extends UnrestrictedProduction>, ? extends UnrestrictedGrammar<? extends UnrestrictedProduction>> create = (grammar, productions) -> new UnrestrictedGrammarImpl(grammar.nonTerminals(), grammar.terminals(), productions, grammar.startSymbol());
         final ProductionBuilder<? extends UnrestrictedProduction> productionBuilder = UnrestrictedProduction.builder();
-        return this.partialBuild((grammar, productions) -> new UnrestrictedGrammarImpl(grammar.nonTerminals(), grammar.terminals(), productions, grammar.startSymbol()), productionBuilder);
+        return this.partialBuild(create, productionBuilder);
     }
 }
