bf08ddfd28cdf7df15f8c037b2d2e202fcd3a36d
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:35:52 +0200

    grammar linear builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/linear/builder/LinearGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/linear/builder/LinearGrammarBuilder.java
@@ -1,13 +1,15 @@
 package org.compilers.grammar.model.grammar.linear.builder;
 
+import org.compilers.grammar.model.grammar.Grammar;
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
-import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
 import org.compilers.grammar.model.grammar.linear.LinearGrammar;
 import org.compilers.grammar.model.grammar.linear.LinearGrammarImpl;
+import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.linear.LinearProduction;
 
 import java.util.Set;
+import java.util.function.BiFunction;
 
 public class LinearGrammarBuilder extends AbstractGrammarBuilder<LinearProduction, LinearGrammar<? extends LinearProduction>> {
     public LinearGrammarBuilder() {
@@ -15,14 +17,8 @@ public class LinearGrammarBuilder extends AbstractGrammarBuilder<LinearProductio
 
     @Override
     public LinearGrammar<? extends LinearProduction> build() {
-        this.partialBuild();
-
-        final ProductionBuilder<? extends LinearProduction> productionBuilder = LinearProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends LinearProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
-        final LinearGrammar<? extends LinearProduction> linearGrammar = new LinearGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
-
-        this.eraseAll();
-
-        return linearGrammar;
+        final BiFunction<Grammar<? extends Production>, Set<? extends LinearProduction>, LinearGrammar<? extends LinearProduction>> create = (grammar, productions) -> new LinearGrammarImpl(grammar.nonTerminals(), grammar.terminals(), productions, grammar.startSymbol());
+        final ProductionBuilder<? extends LinearProduction> productionBuilder = LinearProduction.builder();
+        return this.partialBuild(create, productionBuilder);
     }
 }
