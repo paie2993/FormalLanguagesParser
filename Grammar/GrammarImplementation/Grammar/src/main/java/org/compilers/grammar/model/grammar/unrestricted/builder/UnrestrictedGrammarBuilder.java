957587fc51b3f674e4e16f7958c2a0a18680d0af
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:25:00 +0200

    grammar unrestricted builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/unrestricted/builder/UnrestrictedGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/unrestricted/builder/UnrestrictedGrammarBuilder.java
@@ -1,13 +1,10 @@
 package org.compilers.grammar.model.grammar.unrestricted.builder;
 
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
-import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.unrestricted.UnrestrictedProduction;
 import org.compilers.grammar.model.grammar.unrestricted.UnrestrictedGrammar;
 import org.compilers.grammar.model.grammar.unrestricted.UnrestrictedGrammarImpl;
-
-import java.util.Set;
 
 public class UnrestrictedGrammarBuilder extends AbstractGrammarBuilder<UnrestrictedProduction, UnrestrictedGrammar<? extends UnrestrictedProduction>> {
     public UnrestrictedGrammarBuilder() {
@@ -15,14 +12,7 @@ public class UnrestrictedGrammarBuilder extends AbstractGrammarBuilder<Unrestric
 
     @Override
     public UnrestrictedGrammar<? extends UnrestrictedProduction> build() {
-        this.partialBuild();
-
-        final ProductionBuilder<? extends UnrestrictedProduction> productionBuilder = UnrestrictedProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends UnrestrictedProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
-        final UnrestrictedGrammar<? extends UnrestrictedProduction> unrestrictedGrammar = new UnrestrictedGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
-
-        this.eraseAll();
-
-        return unrestrictedGrammar;
+        final ProductionBuilder<? extends UnrestrictedProduction> productionBuilder = UnrestrictedProduction.builder();
+        return this.partialBuild((grammar, productions) -> new UnrestrictedGrammarImpl(grammar.nonTerminals(), grammar.terminals(), productions, grammar.startSymbol()), productionBuilder);
     }
 }
