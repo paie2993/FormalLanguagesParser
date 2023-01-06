dce9c7071c9e12772455659e7f46cbc9624d5ff2
916-Andrei-Saramet <andreisaramet21@gmail.com>
2023-01-06 15:11:15 +0200

    grammar linear builder implementation update

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/linear/builder/LinearGrammarBuilder.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/model/grammar/linear/builder/LinearGrammarBuilder.java
@@ -1,14 +1,13 @@
 package org.compilers.grammar.model.grammar.linear.builder;
 
 import org.compilers.grammar.model.grammar.builder.AbstractGrammarBuilder;
+import org.compilers.grammar.model.grammar.builder.GrammarBuilder;
 import org.compilers.grammar.model.grammar.linear.LinearGrammar;
 import org.compilers.grammar.model.grammar.linear.LinearGrammarImpl;
-import org.compilers.grammar.model.grammar.production.Production;
 import org.compilers.grammar.model.grammar.production.builder.ProductionBuilder;
 import org.compilers.grammar.model.grammar.production.linear.LinearProduction;
 
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class LinearGrammarBuilder extends AbstractGrammarBuilder<LinearProduction, LinearGrammar<? extends LinearProduction>> {
     public LinearGrammarBuilder() {
@@ -19,19 +18,11 @@ public class LinearGrammarBuilder extends AbstractGrammarBuilder<LinearProductio
         this.partialBuild();
 
         final ProductionBuilder<? extends LinearProduction> productionBuilder = LinearProduction.builder().symbols(this.grammar.nonTerminals(), this.grammar.terminals());
-        final Set<? extends LinearProduction> productions = prepareProductions(this.grammar.productions(), productionBuilder);
+        final Set<? extends LinearProduction> productions = GrammarBuilder.prepareProductions(this.grammar.productions(), productionBuilder);
         final LinearGrammar<? extends LinearProduction> linearGrammar = new LinearGrammarImpl(this.grammar.nonTerminals(), this.grammar.terminals(), productions, this.grammar.startSymbol());
 
         this.eraseAll();
 
         return linearGrammar;
-    }
-
-    private static Set<? extends LinearProduction> prepareProductions(final Set<? extends Production> productions, final ProductionBuilder<? extends LinearProduction> builder) {
-        return productions
-                .stream()
-                .map(builder::production)
-                .map(ProductionBuilder::build)
-                .collect(Collectors.toUnmodifiableSet());
     }
 }
