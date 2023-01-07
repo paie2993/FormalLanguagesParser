99760a21d44d78db6a439b0f7b7435f9d6beec40
Adrian Popescu <adrian.popescu@stud.ubbcluj.ro>
2023-01-07 12:50:56 +0200

    Added tests for Termial.isInstance

--- Grammar/GrammarImplementation/Grammar/src/test/java/org/compilers/grammar/model/vocabulary/nonterminal/TestNonTerminal.java
+++ Grammar/GrammarImplementation/Grammar/src/test/java/org/compilers/grammar/model/vocabulary/nonterminal/TestNonTerminal.java
@@ -1,6 +1,5 @@
 package org.compilers.grammar.model.vocabulary.nonterminal;
 
-import org.compilers.grammar.model.vocabulary.terminal.Terminal;
 import org.compilers.grammar.model.vocabulary.terminal.TerminalImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,7 +10,6 @@ class TestNonTerminal {
     @Test
     void isInstance() {
         final var nonTerminal = new NonTerminalImpl("A");
-
         Assertions.assertTrue(NonTerminal.isInstance(nonTerminal));
     }
 
@@ -19,7 +17,6 @@ class TestNonTerminal {
     @Test
     void isInstanceNot() {
         final var terminal = new TerminalImpl("a");
-
-        Assertions.assertTrue(Terminal.isInstance(terminal));
+        Assertions.assertFalse(NonTerminal.isInstance(terminal));
     }
 }
