1103a1cf742244445435cb89b766cba60db48e9d
Adrian G Popescu <adriang.popescu@itiviti.com>
2023-01-11 09:36:51 +0200

    Simplified ParserOutputWriter formatted ParserOut
    Change-Id: I558e3b9709c4a7cd2e774d1dad68d83d97d4e21e

--- Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/parser/writer/ParserOutputWriter.java
+++ Grammar/GrammarImplementation/Grammar/src/main/java/org/compilers/grammar/parser/writer/ParserOutputWriter.java
@@ -1,35 +1,57 @@
 package org.compilers.grammar.parser.writer;
 
+import org.compilers.grammar.parser.output.ParserOutput;
 import org.compilers.grammar.parser.output.father_sibling_table.entry.TableEntry;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public interface ParserOutputWriter {
-    String COLUMN_SEPARATOR = " ";
 
-    static void writeToFile(final String fileName, final List<? extends TableEntry> output) throws IOException {
-        write(fileName, COLUMN_SEPARATOR, output);
+    static void writeToFile(final String fileName, final ParserOutput output) throws IOException {
+        write(fileName, output);
     }
 
-    static void writeToFile(final String fileName, final String columnSeparator, final List<? extends TableEntry> output) throws IOException {
-        write(fileName, columnSeparator, output);
-    }
+    private static void write(final String fileName, final ParserOutput output) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write("As production string:");
+            writer.newLine();
+            writeList(writer, output.asProductionString());
 
-    private static void write(final String fileName, final String columnSeparator, final List<? extends TableEntry> output) throws IOException {
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
-            output.stream()
-                    .map(entry -> String.format("%s%s%d%s%d", entry.symbol(), columnSeparator, entry.parentIndex(), columnSeparator, entry.rightSiblingIndex()))
-                    .forEach(entry -> {
-                        try {
-                            bufferedWriter.append(entry);
-                            bufferedWriter.newLine();
-                        } catch (IOException ioException) {
-                            throw new RuntimeException(ioException);
-                        }
-                    });
+            writer.write("As derivation sequence:");
+            writer.newLine();
+            writeList(writer, output.asDerivationString());
+
+            writer.write("As father-sibling table:");
+            writer.newLine();
+            writeTable(writer, output.asFatherSiblingTable());
         }
+    }
+
+    private static void writeList(final BufferedWriter writer, final List<?> list) {
+        list.forEach(element -> {
+            try {
+                writer.write(element.toString());
+                writer.newLine();
+            } catch (final IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static void writeTable(final BufferedWriter writer, final List<? extends TableEntry> table) throws IOException {
+        writer.write(String.format("%-20s | %-20s | %-20s | %-20s", "Index", "Info", "Parent", "Right sibling"));
+        final var index = new AtomicInteger(0);
+        table.forEach(entry -> {
+            try {
+                writer.write(String.format("%-20s | %s", index.getAndIncrement(), entry.toString()));
+                writer.newLine();
+            } catch (final IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 }
